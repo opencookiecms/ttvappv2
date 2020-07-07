c72a4bad5557ffe919eeeded14e2b413ea93695b
SYED MOHD AFIQ SYED ABDUL RAHMAN <syed.m.afiq@outlook.com>
2020-07-07 16:15:03 +0800

    upload

--- src/ttvapp/settings.py
+++ src/ttvapp/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1','192.168.0.125','192.168.0.114']
+ALLOWED_HOSTS = ['localhost','127.0.0.1','192.168.0.125','192.168.0.114','intra.ttv.net']
 
 
 # Application definition
